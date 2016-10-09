7ce93fd1c657f190cabf969373a88b6c1399b305
1440590776@qq.com <1440590776@qq.com>
2016-10-09 17:01:16 +0800

    add projects

--- nodejs入门/list.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var Mess = require('./views/index');
-var database = require('./database');
-module.exports = function(req, res) {
-	res.end(new Mess(database.list).render());
-}
--- nodejs入门/query.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var qs = require('querystring');
-var url = require('url');
-
-module.exports = function query(req){
-	let query = url.parse(req.url).query;
-	return qs.parse(query);
-}
